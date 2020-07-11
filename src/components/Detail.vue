2cdecca10aaf17b4bf37f73355eca959e04b800b
chenmo1212 <wx_70a5d5343b674982ac5671f25ca04666@git.code.tencent.com>
2020-07-11 13:39:31 +0800

    detail init2

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -8,7 +8,12 @@
         <div>{{projectName}} - {{chapterName}}</div>
       </div>
     </div>
-    <div id="board"></div>
+    <div id="board">
+      <div class="card" v-for="(item, index) in cardArr">
+        {{item.title}}
+        <!--<span @click="cardPre()">回来</span>-->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,232 +26,284 @@
       return {
         projectName: '马克思',
         chapterName: '导论',
+        cardArr: [
+          {
+          title: 1,
+        }, {
+          title: 2,
+        }, {
+          title: 3,
+        }, {
+          title: 4,
+        }, {
+          title: 5,
+        }, {
+          title: 6,
+        }, {
+          title: 7,
+        },],
       }
     },
     mounted() {
-      /* LikeCarousel (c) 2019 Simone P.M. github.com/simonepm - Licensed MIT */
+      this.cardInit();
 
-      class Carousel {
-
-        constructor(element) {
-
-          this.board = element;
-
-          // add first two cards programmatically
-          this.push();
-          this.push();
-
-          // handle gestures
-          this.handle()
-
-        }
-
-        handle() {
-
-          // list all cards
-          this.cards = this.board.querySelectorAll('.card');
-
-          // get top card
-          this.topCard = this.cards[this.cards.length - 1];
-
-          // get next card
-          this.nextCard = this.cards[this.cards.length - 2];
-
-          // if at least one card is present
-          if (this.cards.length > 0) {
-
-            // set default top card position and scale
-            this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)';
-
-            // destroy previous Hammer instance, if present
-            if (this.hammer) this.hammer.destroy();
-
-            // listen for tap and pan gestures on top card
-            this.hammer = new Hammer(this.topCard);
-            this.hammer.add(new Hammer.Tap());
-            this.hammer.add(new Hammer.Pan({position: Hammer.position_ALL, threshold: 0}))
-
-            // pass events data to custom callbacks
-            this.hammer.on('tap', (e) => {
-              this.onTap(e)
-            });
-            this.hammer.on('pan', (e) => {
-              this.onPan(e)
-            })
-
-          }
-
-        }
-
-        onTap(e) {
-
-          // get finger position on top card
-          let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth
-
-          // get degree of Y rotation (+/-15 degrees)
-          let rotateY = 15 * (propX < 0.05 ? -1 : 1);
-
-          // change the transition property
-          this.topCard.style.transition = 'transform 100ms ease-out'
-
-          // rotate
-          this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(' + rotateY + 'deg) scale(1)'
-
-          // wait transition end
-          setTimeout(() => {
-            // reset transform properties
-            this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)'
-          }, 100)
-
-        }
-
-        onPan(e) {
-
-          if (!this.isPanning) {
-
-            this.isPanning = true
-
-            // remove transition properties
-            this.topCard.style.transition = null
-            if (this.nextCard) this.nextCard.style.transition = null
-
-            // get top card coordinates in pixels
-            let style = window.getComputedStyle(this.topCard)
-            let mx = style.transform.match(/^matrix\((.+)\)$/)
-            this.startPosX = mx ? parseFloat(mx[1].split(', ')[4]) : 0
-            this.startPosY = mx ? parseFloat(mx[1].split(', ')[5]) : 0
-
-            // get top card bounds
-            let bounds = this.topCard.getBoundingClientRect()
-
-            // get finger position on top card, top (1) or bottom (-1)
-            this.isDraggingFrom = (e.center.y - bounds.top) > this.topCard.clientHeight / 2 ? -1 : 1
-
-          }
-
-          // calculate new coordinates
-          let posX = e.deltaX + this.startPosX
-          let posY = e.deltaY + this.startPosY
-
-          // get ratio between swiped pixels and the axes
-          let propX = e.deltaX / this.board.clientWidth
-          let propY = e.deltaY / this.board.clientHeight
-
-          // get swipe direction, left (-1) or right (1)
-          let dirX = e.deltaX < 0 ? -1 : 1
-
-          // calculate rotation, between 0 and +/- 45 deg
-          let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45
-
-          // calculate scale ratio, between 95 and 100 %
-          let scale = (95 + (5 * Math.abs(propX))) / 100
-
-          // move top card
-          this.topCard.style.transform = 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg) rotateY(0deg) scale(1)'
-
-          // scale next card
-          if (this.nextCard) {
-            this.nextCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(' + scale + ')';
-            this.nextCard.style.backgroundColor = '#f4f6f8';
-          }
-
-          if (e.isFinal) {
-
-            this.isPanning = false
-
-            let successful = false
-
-            // set back transition properties
-            this.topCard.style.transition = 'transform 200ms ease-out'
-            if (this.nextCard) this.nextCard.style.transition = 'transform 100ms linear'
-
-            // check threshold
-            if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
-
-              successful = true
-              // get right border position
-              posX = this.board.clientWidth
-
-            } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
-
-              successful = true
-              // get left border position
-              posX = -(this.board.clientWidth + this.topCard.clientWidth)
-
-            } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
-
-              successful = true
-              // get top border position
-              posY = -(this.board.clientHeight + this.topCard.clientHeight)
-
-            }
-
-            if (successful) {
-
-              // throw card in the chosen direction
-              this.topCard.style.transform = 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
-
-              // wait transition end
-              setTimeout(() => {
-                // remove swiped card
-                this.board.removeChild(this.topCard);
-                // add new card
-                this.push()
-                // handle gestures on new top card
-                this.handle()
-              }, 200)
-
-            } else {
-
-              // reset cards position
-              this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)';
-              if (this.nextCard) this.nextCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(0.95)'
-            }
-          }
-        }
-
-        push() {
-
-          let card = document.createElement('div');
-          // var card = document.getElementById('card');
-          // console.log(card);
-          card.style.height = '85%';
-          card.style.width = '90%';
-          card.style.position = 'absolute';
-          card.style.top = '5%';
-          card.style.left = '5%';
-          card.style.borderRadius = "11px";
-          card.style.background = '#f4f6f8';
-          card.style.border = "1px solid #fff";
-          card.style.boxShadow = "5px 5px 8px #ebebeb, -5px -5px 8px #ffffff";
-          card.classList.add('card');
-          //(parseInt(3*Math.random())+1)为随机1-3随机数，需要几的随机数改前面的数字3即可，这里images文件夹图片就放了三张，可以多放几张，这样重复率会降低
-          // card.style.backgroundImage = "url('../images/" + (parseInt(6 * Math.random()) + 1) + ".jpg"
-
-
-          if (this.board.firstChild) {
-            this.board.insertBefore(card, this.board.firstChild)
-          } else {
-            this.board.append(card)
-          }
-        }
-
-      }
-
-      let board = document.querySelector('#board')
-
-      let carousel = new Carousel(board)
+      this.cardArr = this.cardArr.reverse();
     },
     methods: {
       backChapter() {
         this.$router.push({name: 'chapter'});
       },
+
+      // cardPre() {
+      //   // const board = document.getElementById("board");
+      //   // const cards = board.querySelectorAll('.card');
+      //   // console.log(cards.length);
+      //   // let tempArr = this.cardArr.slice(0, cards.length + 1);
+      //   // console.log(tempArr);
+      //   // this.cardArr = tempArr;
+      //   // console.log(this.cardArr);
+      //   // // this.cardInit();
+      //
+      //   this.cardArr.push({title: "插入的"})
+      // },
+
+      // 卡片布局
+      cardInit() {
+        /* LikeCarousel (c) 2019 Simone P.M. github.com/simonepm - Licensed MIT */
+
+        class Carousel {
+
+          constructor(element) {
+
+            this.board = element;
+
+            // add first two cards programmatically
+            // this.push();
+            // this.push();
+
+            // handle gestures
+            this.handle()
+
+          }
+
+          handle() {
+
+            // list all cards
+            this.cards = this.board.querySelectorAll('.card');
+
+            // get top card
+            this.topCard = this.cards[this.cards.length - 1];
+
+            // get next card
+            this.nextCard = this.cards[this.cards.length - 2];
+
+            // if at least one card is present
+            if (this.cards.length > 0) {
+
+              // set default top card position and scale
+              this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)';
+
+              // destroy previous Hammer instance, if present
+              if (this.hammer) this.hammer.destroy();
+
+              // listen for tap and pan gestures on top card
+              this.hammer = new Hammer(this.topCard);
+              this.hammer.add(new Hammer.Tap());
+              this.hammer.add(new Hammer.Pan({position: Hammer.position_ALL, threshold: 0}))
+
+              // pass events data to custom callbacks
+              this.hammer.on('tap', (e) => {
+                this.onTap(e)
+              });
+              this.hammer.on('pan', (e) => {
+                this.onPan(e)
+              })
+
+            }
+
+          }
+
+          onTap(e) {
+
+            // get finger position on top card
+            let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth;
+
+            // get degree of Y rotation (+/-15 degrees)
+            let rotateY = 15 * (propX < 0.05 ? -1 : 1);
+
+            // change the transition property
+            this.topCard.style.transition = 'transform 100ms ease-out';
+
+            // rotate
+            this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(' + rotateY + 'deg) scale(1)'
+
+            // wait transition end
+            setTimeout(() => {
+              // reset transform properties
+              this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)'
+            }, 100)
+
+          }
+
+          onPan(e) {
+
+            if (!this.isPanning) {
+
+              this.isPanning = true;
+
+              // remove transition properties
+              this.topCard.style.transition = null;
+              if (this.nextCard) this.nextCard.style.transition = null;
+
+              // get top card coordinates in pixels
+              let style = window.getComputedStyle(this.topCard);
+              let mx = style.transform.match(/^matrix\((.+)\)$/);
+              this.startPosX = mx ? parseFloat(mx[1].split(', ')[4]) : 0;
+              this.startPosY = mx ? parseFloat(mx[1].split(', ')[5]) : 0;
+
+              // get top card bounds
+              let bounds = this.topCard.getBoundingClientRect();
+
+              // get finger position on top card, top (1) or bottom (-1)
+              this.isDraggingFrom = (e.center.y - bounds.top) > this.topCard.clientHeight / 2 ? -1 : 1
+
+            }
+
+            // calculate new coordinates
+            let posX = e.deltaX + this.startPosX;
+            let posY = e.deltaY + this.startPosY;
+
+            // get ratio between swiped pixels and the axes
+            let propX = e.deltaX / this.board.clientWidth;
+            let propY = e.deltaY / this.board.clientHeight;
+
+            // get swipe direction, left (-1) or right (1)
+            let dirX = e.deltaX < 0 ? -1 : 1;
+
+            // calculate rotation, between 0 and +/- 45 deg
+            let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45;
+
+            // calculate scale ratio, between 95 and 100 %
+            let scale = (95 + (5 * Math.abs(propX))) / 100;
+
+            // move top card
+            this.topCard.style.transform = 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg) rotateY(0deg) scale(1)'
+
+            // scale next card
+            if (this.nextCard) {
+              this.nextCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(' + scale + ')';
+              this.nextCard.style.backgroundColor = '#f4f6f8';
+            }
+
+            if (e.isFinal) {
+
+              this.isPanning = false;
+
+              let successful = false;
+
+              // set back transition properties
+              this.topCard.style.transition = 'transform 200ms ease-out'
+              if (this.nextCard) this.nextCard.style.transition = 'transform 100ms linear'
+
+              // check threshold
+              if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
+
+                successful = true;
+                // get right border position
+                posX = this.board.clientWidth
+
+              } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
+
+                successful = true;
+                // get left border position
+                posX = -(this.board.clientWidth + this.topCard.clientWidth)
+
+              } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
+
+                successful = true;
+                // get top border position
+                posY = -(this.board.clientHeight + this.topCard.clientHeight)
+
+              }
+
+              if (successful) {
+
+                // throw card in the chosen direction
+                this.topCard.style.transform = 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)'
+
+                // console.log(this.cards.length)
+
+                // 根据card的长度来判断是否留下
+                if (this.cards.length > 1) {
+                  const that = this;
+                  // wait transition end
+                  setTimeout(() => {
+                    // remove swiped card
+                    this.board.removeChild(this.topCard);
+                    // add new card
+                    // this.push()
+                    // handle gestures on new top card
+                    this.handle();
+                  }, 200)
+                } else {
+                  setTimeout(() => {
+                    // handle gestures on new top card
+                    this.handle()
+                  }, 200)
+                }
+
+              } else {
+
+                // reset cards position
+                this.topCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(1)';
+                if (this.nextCard) this.nextCard.style.transform = 'translateX(0%) translateY(0%) rotate(0deg) rotateY(0deg) scale(0.95)'
+              }
+            }
+          }
+
+          // push() {
+          //
+          //   let card = document.createElement('div');
+          //   // var card = document.getElementById('card');
+          //   // console.log(card);
+          //   card.style.height = '85%';
+          //   card.style.width = '90%';
+          //   card.style.position = 'absolute';
+          //   card.style.top = '5%';
+          //   card.style.left = '5%';
+          //   card.style.borderRadius = "11px";
+          //   card.style.background = '#f4f6f8';
+          //   card.style.border = "1px solid #fff";
+          //   card.style.boxShadow = "5px 5px 8px #ebebeb, -5px -5px 8px #ffffff";
+          //   card.classList.add('card');
+          //   //(parseInt(3*Math.random())+1)为随机1-3随机数，需要几的随机数改前面的数字3即可，这里images文件夹图片就放了三张，可以多放几张，这样重复率会降低
+          //   // card.style.backgroundImage = "url('../images/" + (parseInt(6 * Math.random()) + 1) + ".jpg"
+          //
+          //
+          //   if (this.board.firstChild) {
+          //     this.board.insertBefore(card, this.board.firstChild)
+          //   } else {
+          //     this.board.append(card)
+          //   }
+          // }
+
+        }
+
+        let board = document.querySelector('#board')
+
+        let carousel = new Carousel(board)
+      }
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+  .Detail {
+    background-color: #f4f6f8;
+  }
+
   .header {
     height: 60px;
     line-height: 60px;
@@ -279,10 +336,9 @@
 
   #board {
     width: 100%;
-    height: 92vh;
+    height: calc(100vh - 60px);
     position: relative;
     overflow: hidden;
-    background-color: #f4f6f8;
   }
 
   .card {
@@ -296,5 +352,12 @@
     box-shadow: 5px 5px 8px #ebebeb, -5px -5px 8px #ffffff;
     background-color: #f4f6f8;
     transform: translateX(0%) translateY(0%) scale(0.95);
+  }
+
+  /*往右切换*/
+  .nextCard {
+    @extend .card;
+    transform: translateX(328px) translateY(2px) rotate(39.36deg) rotateY(0deg) scale(1) !important;
+    transition: 500ms !important;
   }
 </style>
